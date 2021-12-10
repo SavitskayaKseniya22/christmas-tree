e2eb800df4e4709baf3e901c6b018039f5a59bf8
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2021-12-11 00:30:18 +0300

    feat: add toy-cards

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,2 +1,10 @@
 import "./styles.scss";
 import "./normalize.scss";
+import data from "./data";
+import printAllCards from "./toys";
+
+const mainContainer = document.querySelector(".toys-container");
+
+if (mainContainer) {
+  printAllCards(data, mainContainer);
+}
---
+++ christmas-task/src/toys.ts
@@ -0,0 +1,87 @@
+class Card {
+  name: string;
+
+  count: string;
+
+  year: string;
+
+  shape: string;
+
+  color: string;
+
+  size: string;
+
+  favorite: boolean;
+
+  num: string;
+
+  src: string;
+
+  element: string;
+
+  favoriteString: string;
+
+  constructor(obj: {
+    num: string;
+    name: string;
+    count: string;
+    year: string;
+    shape: string;
+    color: string;
+    size: string;
+    favorite: boolean;
+  }) {
+    this.name = obj.name;
+    this.count = obj.count;
+    this.year = obj.year;
+    this.shape = obj.shape;
+    this.color = obj.color;
+    this.size = obj.size;
+    this.favorite = obj.favorite;
+
+    this.num = obj.num;
+    this.src = `./assets/toys/${this.num}.png`;
+    if (this.favorite) {
+      this.favoriteString = "да";
+    } else {
+      this.favoriteString = "нет";
+    }
+    this.element = `<div class="toy-item">
+    <h4>${this.name}</h4>
+    <img class="toy-image" src=${this.src} alt="toy" />
+    <ul>
+      <li>Количество: <span>${this.count}</span></li>
+      <li>Год покупки: <span>${this.year}</span></li>
+      <li>Форма: <span>${this.shape}</span></li>
+      <li>Цвет: <span>${this.color}</span></li>
+      <li>Размер: <span>${this.size}</span></li>
+      <li>Любимая: <span>${this.favoriteString}</span></li>
+    </ul>
+    <img class="star-image" src="./assets/svg/star-empty.svg" alt="star" />
+  </div>`;
+  }
+}
+
+function printCard(obj: { element: string }, container: Element) {
+  // eslint-disable-next-line no-param-reassign
+  container.innerHTML += obj.element;
+}
+
+export default function printAllCards(
+  arr: {
+    num: string;
+    name: string;
+    count: string;
+    year: string;
+    shape: string;
+    color: string;
+    size: string;
+    favorite: boolean;
+  }[],
+  container: Element
+) {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of arr) {
+    printCard(new Card(item), container);
+  }
+}
