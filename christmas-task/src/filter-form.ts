47a178dfb1080b51edad82add9066970a6548e8c
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2021-12-13 18:44:00 +0300

    refactor: filter basics for shape

--- christmas-task/src/filter-form.ts
+++ christmas-task/src/filter-form.ts
@@ -42,18 +42,51 @@ const objColor: IObjColor = {
   "green-color": "зелёный",
 };
 
+const filters = {
+  color: {
+    "white-color": false,
+    "yellow-color": false,
+    "red-color": false,
+    "blue-color": false,
+    "green-color": false,
+  },
+  shape: {
+    "round-shape": false,
+    "bell-shape": false,
+    "cone-shape": false,
+    "snowflake-shape": false,
+    "custom-shape": false,
+  },
+  favorite: false,
+  size: {
+    "big-size": false,
+    "middle-size": false,
+    "small-size": false,
+  },
+};
+
 document.addEventListener("click", (e: Event) => {
   let toyCollection = Array.from(document.querySelectorAll(".toy-item"));
 
   if ((e.target as HTMLElement).closest(".shape-label")) {
     // eslint-disable-next-line no-restricted-syntax
     for (const elem of Object.keys(objShape)) {
-      if ((e.target as HTMLElement).closest(`[for=${elem}]`)) {
-        toyCollection = toyCollection.filter(
-          (item) =>
-            item.querySelector(".shape-toy")?.textContent === objShape[elem]
-        );
-        console.log(toyCollection);
+      const element = (e.target as HTMLElement).closest(`[for=${elem}]`);
+      if (element) {
+        if (!filters.shape[elem]) {
+          alert(1);
+          filters.shape[elem] = true;
+          element.classList.add("active");
+          /* toyCollection = toyCollection.filter(
+            (item) =>
+              item.querySelector(".shape-toy")?.textContent === objShape[elem]
+          );
+          console.log(toyCollection); */
+        } else {
+          alert(2);
+          filters.shape[elem] = false;
+          element.classList.remove("active");
+        }
       }
     }
   }
@@ -91,3 +124,38 @@ document.addEventListener("click", (e: Event) => {
     console.log(toyCollection);
   }
 });
+
+/*
+
+document.addEventListener("click", (e: Event) => {
+  let toyCollection = Array.from(document.querySelectorAll(".toy-item"));
+  let activeFilter = [];
+
+  if ((e.target as HTMLElement).closest(".shape-label")) {
+    if (!filters.shape) {
+      filters.shape = true;
+      console.log(filters);
+    }
+  }
+
+  if ((e.target as HTMLElement).closest(".size-label")) {
+    if (!filters.size) {
+      filters.size = true;
+      console.log(filters);
+    }
+  }
+
+  if ((e.target as HTMLElement).closest(".color-label")) {
+    if (!filters.color) {
+      filters.color = true;
+      console.log(filters);
+    }
+  }
+
+  if ((e.target as HTMLElement).closest(".favorite-label")) {
+    if (!filters.color) {
+      filters.favorite = true;
+      console.log(filters);
+    }
+  }
+}); */
