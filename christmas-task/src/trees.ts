1da2bc7bb940d0eef90393f9fa3a64295b4f8909
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2021-12-24 17:59:22 +0300

    feat: add some logic for game settings

--- christmas-task/src/trees.ts
+++ christmas-task/src/trees.ts
@@ -18,32 +18,6 @@ if (collection && collection.length > 0) {
   }
 }
 
-const resultScreen = document.querySelector(".result-screen") as HTMLDivElement;
-const bgCollection = document.querySelectorAll("input[name='bg']");
-
-bgCollection.forEach((element) => {
-  element.addEventListener("click", function (e: Event) {
-    const number = (e.target as HTMLInputElement).value;
-    resultScreen.className = "result-screen bg";
-    resultScreen.classList.add(`bg${number}`);
-    //resultScreen.style.backgroundImage = `url("./assets/bg/${number}.jpg") cover center`;
-  });
-});
-
-const treeCollection = document.querySelectorAll("input[name='tree']");
-
-treeCollection.forEach((element) => {
-  element.addEventListener("click", function (e: Event) {
-    const number = (e.target as HTMLInputElement).value;
-
-    const treeImg = document.createElement("img");
-    treeImg.src = `./assets/tree/${number}.png`;
-    treeImg.classList.add("tree-img");
-    resultScreen.innerHTML = "";
-    resultScreen.append(treeImg);
-  });
-});
-
 class Game {
   bg: string;
   tree: string;
@@ -71,24 +45,54 @@ class Settings {
   snow: boolean;
   tree: string;
   bg: string;
-  checkbox: HTMLInputElement;
+  checkboxMusic: HTMLInputElement;
+  checkboxSnow: HTMLInputElement;
   audio: HTMLAudioElement;
-  constructor(music = false, snow = false, bg = "bg1", tree = "./assets/tree/1.png") {
+  bgCollection: NodeListOf<Element>;
+  resultScreen: HTMLDivElement;
+  treeCollection: NodeListOf<Element>;
+
+  constructor(music = false, snow = false, bg = "bg1", tree = "1") {
     this.music = music;
     this.snow = snow;
     this.bg = bg;
     this.tree = tree;
-    this.checkbox = document.querySelector("#toggle-button-music");
+    this.checkboxMusic = document.querySelector("#toggle-button-music");
     this.audio = document.querySelector("audio");
+    this.checkboxSnow = document.querySelector("#toggle-button-snow");
+    this.bgCollection = document.querySelectorAll("input[name='bg']");
+    this.resultScreen = document.querySelector(".result-screen");
+    this.treeCollection = document.querySelectorAll("input[name='tree']");
 
-    if (this.music) {
-      this.checkbox.checked = true;
-      this.audio.play();
-    }
+    this.changeMusic(this.music);
+    this.changeBg(this.bg);
+    this.changeSnow(this.snow);
+    this.changeTree(this.tree);
+
+    this.checkboxMusic.addEventListener("change", () => {
+      this.changeMusic(this.checkboxMusic.checked);
+    });
+    this.checkboxSnow.addEventListener("change", () => {
+      this.changeSnow(this.checkboxSnow.checked);
+    });
+
+    this.bgCollection.forEach((element) => {
+      element.addEventListener("click", (e: Event) => {
+        this.changeBg((e.target as HTMLInputElement).value);
+      });
+    });
+
+    this.treeCollection.forEach((element) => {
+      element.addEventListener("click", (e: Event) => {
+        this.changeTree((e.target as HTMLInputElement).value);
+      });
+    });
   }
   changeMusic(value: boolean) {
     this.music = value;
+    this.saveSettings();
     if (this.music) {
+      this.checkboxMusic.checked = true;
       this.audio.play();
     } else {
       this.audio.pause();
@@ -97,12 +101,42 @@ class Settings {
 
   changeSnow(value: boolean) {
     this.snow = value;
+    this.saveSettings();
+    if (this.snow) {
+      this.checkboxSnow.checked = true;
+    } else {
+    }
+  }
+  changeBg(value: string) {
+    this.bg = value;
+    this.saveCheckedState(value);
+    this.saveSettings();
+    this.resultScreen.className = "result-screen bg";
+    this.resultScreen.classList.add(value);
+  }
+  changeTree(value: string) {
+    this.tree = value;
+    this.saveCheckedState(value);
+    this.saveSettings();
+    const treeImg = document.createElement("img");
+    treeImg.src = `./assets/tree/${value}.png`;
+    treeImg.classList.add("tree-img");
+    this.resultScreen.innerHTML = "";
+    this.resultScreen.append(treeImg);
+  }
+  saveCheckedState(value: string) {
+    const item = document.querySelector(`input[value="${value}"]`) as HTMLInputElement;
+    item.checked = true;
+  }
+
+  saveSettings() {
+    window.localStorage.setItem("gameSettings", JSON.stringify([this.music, this.snow, this.bg, this.tree]));
   }
 }
-
-const settings = new Settings();
-
-const audioCheckbox = document.querySelector("#toggle-button-music");
-audioCheckbox.addEventListener("change", function () {
-  settings.changeMusic((audioCheckbox as HTMLInputElement).checked);
-});
+let settings: Settings;
+if (window.localStorage.getItem("gameSettings")) {
+  const arr = JSON.parse(window.localStorage.getItem("gameSettings"));
+  settings = new Settings(arr[0], arr[1], arr[2], arr[3]);
+} else {
+  settings = new Settings();
+}
