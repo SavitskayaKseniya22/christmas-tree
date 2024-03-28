d34455922f6a6f94320a31f31f0a3bf92d0fbf96
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-03-28 18:21:03 +0200

    refactor: styles

--- src/pages/game-page/game-page.ts
+++ src/pages/game-page/game-page.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import './game-page.scss';
 import './lib/game-controls/game-controls';
 import './lib/game-field/game-field';
@@ -7,7 +9,7 @@ export class GamePage extends HTMLElement {
   static observedAttributes = ['rerender'];
   constructor() {
     super();
-    this.className = 'main game-page';
+    this.className = 'main page_game';
   }
 
   render(): void {
@@ -29,7 +31,7 @@ export class GamePage extends HTMLElement {
     `;
   }
 
-  setCoords(element: HTMLElement, e: Event, block: Element): void {
+  static setCoords(element: HTMLElement, e: Event, block: Element): void {
     const { pageX, pageY } = e as MouseEvent;
     element.style.left = ` ${pageX - (block.getBoundingClientRect().left + 20 + window.scrollX)}px`;
     element.style.top = `${pageY - (block.getBoundingClientRect().top + 20 + window.scrollY)}px`;
@@ -39,113 +41,127 @@ export class GamePage extends HTMLElement {
     this.render();
   }
 
+  static drop(event: DragEvent) {
+    const target = event.target as HTMLElement;
+
+    if (
+      target.tagName === 'AREA' &&
+      event.dataTransfer !== null &&
+      event.dataTransfer.effectAllowed !== 'uninitialized'
+    ) {
+      const { num, parent } = JSON.parse(event.dataTransfer.getData('text'));
+
+      const draggableElement = document.querySelector(
+        `.${parent} img[data-num="${num}"]`
+      );
+      const dropzone = document.querySelector('.game-field__container');
+
+      if (draggableElement !== null && dropzone !== null) {
+        if (parent === 'toy_preview') {
+          const draggableElementDup =
+            draggableElement.cloneNode() as HTMLElement;
+          draggableElementDup.classList.add('toy-image__separate');
+
+          GamePage.setCoords(
+            draggableElementDup,
+            event,
+            dropzone as HTMLElement
+          );
+
+          dropzone.append(draggableElementDup);
+          const count = draggableElement.getAttribute('data-count');
+
+          if (count !== null && draggableElement.parentElement !== null) {
+            draggableElement.parentElement.setAttribute(
+              'data-count',
+              String(+count - 1)
+            );
+          }
+
+          draggableElementDup.addEventListener('dblclick', () => {
+            draggableElementDup.remove();
+
+            const parentElement = document.querySelector(
+              `.${parent}[data-num="${num}"]`
+            );
+
+            if (count !== null && parentElement !== null) {
+              const parentCount = parentElement.getAttribute('data-count');
+              if (parentCount !== null) {
+                parentElement.setAttribute(
+                  'data-count',
+                  String(+parentCount + 1)
+                );
+              }
+            }
+          });
+        } else if (parent === 'game-field__container') {
+          GamePage.setCoords(
+            draggableElement as HTMLElement,
+            event,
+            dropzone as HTMLElement
+          );
+        }
+      }
+    }
+  }
+
+  static dragover(event: DragEvent) {
+    const target = event.target as HTMLElement;
+
+    if (target.tagName === 'AREA' && event.dataTransfer !== null) {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'copy';
+    }
+  }
+
+  static dragstart(event: DragEvent) {
+    if (
+      event.target !== null &&
+      (event.target as HTMLElement).classList.contains('toy-image') &&
+      event.dataTransfer !== null
+    ) {
+      event.dataTransfer.clearData();
+      const element = event.target as HTMLImageElement;
+      const num = element.dataset.num;
+
+      event.dataTransfer.effectAllowed = 'copy';
+      if (num !== undefined) {
+        event.dataTransfer.setData(
+          'text/plain',
+          JSON.stringify({
+            num,
+            parent: element.parentElement?.className,
+          })
+        );
+
+        const img = element;
+
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        if (ctx !== null) {
+          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        }
+
+        event.dataTransfer.setDragImage(canvas, 10, 10);
+      }
+    }
+  }
+
+  disconnectedCallback(): void {
+    this.removeEventListener('dragstart', GamePage.dragstart);
+    this.removeEventListener('dragover', GamePage.dragover);
+    document.removeEventListener('drop', GamePage.drop);
+  }
+
   connectedCallback(): void {
     this.render();
 
-    this.addEventListener('dragover', (event) => {
-      const target = event.target as HTMLElement;
-
-      if (target.tagName === 'AREA' && event.dataTransfer !== null) {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'copy';
-      }
-    });
-
-    this.addEventListener('dragstart', (event) => {
-      if (
-        event.target !== null &&
-        (event.target as HTMLElement).classList.contains('toy-image') &&
-        event.dataTransfer !== null
-      ) {
-        event.dataTransfer.clearData();
-        const element = event.target as HTMLImageElement;
-        const num = element.dataset.num;
-
-        event.dataTransfer.effectAllowed = 'copy';
-        if (num !== undefined) {
-          event.dataTransfer.setData(
-            'text/plain',
-            JSON.stringify({
-              num,
-              parent: element.parentElement?.className,
-            })
-          );
-
-          const img = element;
-
-          const canvas = document.createElement('canvas');
-          const ctx = canvas.getContext('2d');
-          canvas.width = img.width;
-          canvas.height = img.height;
-          if (ctx !== null) {
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-          }
-
-          event.dataTransfer.setDragImage(canvas, 10, 10);
-        }
-      }
-    });
-
-    document.addEventListener('drop', (event) => {
-      const target = event.target as HTMLElement;
-
-      if (
-        target.tagName === 'AREA' &&
-        event.dataTransfer !== null &&
-        event.dataTransfer.effectAllowed !== 'uninitialized'
-      ) {
-        const { num, parent } = JSON.parse(event.dataTransfer.getData('text'));
-
-        const draggableElement = document.querySelector(
-          `.${parent} img[data-num="${num}"]`
-        );
-        const dropzone = this.querySelector('.game-field__container');
-
-        if (draggableElement !== null && dropzone !== null) {
-          if (parent === 'toy_preview') {
-            const draggableElementDup =
-              draggableElement.cloneNode() as HTMLElement;
-            draggableElementDup.classList.add('toy-image__separate');
-
-            this.setCoords(draggableElementDup, event, dropzone as HTMLElement);
-
-            dropzone.append(draggableElementDup);
-            const count = draggableElement.getAttribute('data-count');
-
-            if (count !== null && draggableElement.parentElement !== null) {
-              draggableElement.parentElement.setAttribute(
-                'data-count',
-                String(+count - 1)
-              );
-            }
-
-            draggableElementDup.addEventListener('dblclick', () => {
-              draggableElementDup.remove();
-
-              const parentElement = document.querySelector(
-                `.${parent}[data-num="${num}"]`
-              );
-
-              if (count !== null && parentElement !== null) {
-                const parentCount = parentElement.getAttribute('data-count');
-                if (parentCount !== null) {
-                  parentElement.setAttribute(
-                    'data-count',
-                    String(+parentCount + 1)
-                  );
-                }
-              }
-            });
-          } else if (parent === 'game-field__container') {
-            this.setCoords(
-              draggableElement as HTMLElement,
-              event,
-              dropzone as HTMLElement
-            );
-          }
-        }
-      }
-    });
+    this.addEventListener('dragover', GamePage.dragover);
+    this.addEventListener('dragstart', GamePage.dragstart);
+    document.addEventListener('drop', GamePage.drop);
   }
 }
 
--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -3,16 +3,17 @@ import './main-page.scss';
 export class MainPage extends HTMLElement {
   constructor() {
     super();
-    this.className = 'main';
+    this.className = 'main page_main';
   }
 
   render(): void {
     this.insertAdjacentHTML(
       'afterbegin',
       `
-      <button class="start-game">
-        <a href="/#toys">Наряжать ёлку!</a>
-      </button>
+      <div class="page-main__container">
+        <p>It's so long until Christmas... <br/> If you're tired of waiting, you can decorate the Christmas tree right now.</p>
+        <a class="start-game" href="/#toys" title="Start decorating">Start decorating</a>
+      </div>
     `
     );
   }
